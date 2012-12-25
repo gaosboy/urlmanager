176c490a4eeec2a0e903b34ab66b01aff6d649b5
gaosboy <gaosboy@gmail.com>
2012-12-25 22:30:25 +0800

    coding demo

--- Demo/UMDemoViewController.m
+++ Demo/UMDemoViewController.m
@@ -19,6 +19,52 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor lightGrayColor];
     self.navigationItem.title = [self.params objectForKey:@"title"];
+    
+    UIBarButtonItem *btnItem = [[UIBarButtonItem alloc] initWithTitle:@"侧栏导航"
+                                                                style:UIBarButtonItemStylePlain
+                                                               target:self.slideNavigator
+                                                               action:@selector(slideButtonClicked)];
+    self.navigationItem.leftBarButtonItem = btnItem;
+    
+    UIButton *btnA = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btnA.frame = CGRectMake(10.0f, 10.0f, 300.0f, 44.0f);
+    [btnA setTitle:@"um://demo?title=openfrombtn&keya=valuea&keyb=valueb" forState:UIControlStateNormal];
+    [btnA addTarget:self action:@selector(open:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btnA];
+    
+    UIButton *btnB = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btnB.frame = CGRectMake(10.0f, 60.0f, 300.0f, 44.0f);
+    [btnB setTitle:@"bad://donotopen/wrong/path/?notopen=1" forState:UIControlStateNormal];
+    [btnB addTarget:self action:@selector(open:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btnB];
+    
+    NSLog(@"self url:%@", self.url.absoluteString);
+    NSLog(@"Params:%@", self.params);
+}
+
+- (void)open:(UIButton *)btn
+{
+    [self.navigator openURL:[NSURL URLWithString:btn.titleLabel.text]];
+}
+
+- (void)openedFromViewControllerWithURL:(NSURL *)aUrl
+{
+    self.navigationItem.leftBarButtonItem = self.navigationItem.backBarButtonItem;
+    NSLog(@"Opend From:%@", aUrl.absoluteString);
+}
+
+- (BOOL)shouldOpenViewControllerWithURL:(NSURL *)aUrl
+{
+    // if it will open bad://donotopen/wrong/path/?notopen=1
+    if ([@"bad" isEqualToString:[aUrl scheme]]
+        || [@"donotopen" isEqualToString:[aUrl host]]
+        || [@"1" isEqualToString:[[aUrl params] objectForKey:@"notopen"]]
+        || [[aUrl path] containsString:@"/wrong/"]
+        ) {
+        NSLog(@"do not open.");
+        return NO;
+    }
+    return YES;
 }
 
 @end
